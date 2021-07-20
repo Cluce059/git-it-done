950d7f72e1f9b3a1c97105fe7668ea1026c5b2a7
Caroline <Cluce059@gmail.com>
2021-07-20 17:10:41 -0400

    displays warning if more than 30 repos

--- module-6-starter/assets/js/single.js
+++ module-6-starter/assets/js/single.js
@@ -1,5 +1,7 @@
 //used to append all of these issueEls from the dom onto the actual web[age]
 var isssueContainerEl = document.querySelector("#issues-container");
+//container for limit warning
+var limitWarningEl = document.querySelector("#limit-warning");
 var getRepoIssues = function(repo){
     //add the ? stmnt at the end to change the order they are listed in
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -7,7 +9,11 @@ var getRepoIssues = function(repo){
         if(response.ok){
             response.json().then(function(data){
                 displayIssues(data);
-            })
+            //check if api has paginated issues
+            if(response.headers.get("Link")){
+                displayWarning(repo);
+            }
+            });
         }
         else{
             alert("There was a problem with that request!");
@@ -48,4 +54,14 @@ var displayIssues = function(issues){
         isssueContainerEl.appendChild(issueEl);
         }
 };
+var displayWarning = function(repo){
+    //add text to container
+    limitWarningEl.textContent = "To see more than 30 repos, vitit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See more issues on Github.com";
+    linkEl.setAttribute("href", "https://github.com/" +repo +"/issues");
+    linkEl.setAttribute("target", "_blank");
+    limitWarningEl.appendChild(linkEl);
+};
+
 getRepoIssues("facebook/react");
